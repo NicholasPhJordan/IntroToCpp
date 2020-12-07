75d5e92c08cb689b4622d1769d23a56e540e2e90
Nicholas Jordan <NicholasPhJordan@gmail.com>
2020-12-07 16:58:56 -0600

    Made a number guessing game!!

--- IntroToCpp/Main.cpp
+++ IntroToCpp/Main.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 
-void FizzBuzz(int num)
-{
-	std::cout << "Type a number: ";
-	std::cin >> num;
-	for (int i = 0; i <= num; i++)
-	{
-		if (i % 3 == false && i % 5 == false)
-		{
-			std::cout << "fizz-buzz " << i << std::endl;
-		}
-		else if (i % 3 == false)
-		{
-			std::cout << "fizz " << i << std::endl;
-		}
-		else if (i % 5 == false)
-		{
-			std::cout << "buzz " << i << std::endl;
-		}
-	}
-}
+
 
 int main()
 {
-	int num = 0;
+	std::cout << "Think of a number between 0 and 100" << std::endl;
+	system("pause");
+	system("cls");
 
-	FizzBuzz(num);
+	int min = 0;
+	int max = 100;
+	int input = 0;
+
+	while (min <= max && input != 3)
+	{
+		int guess = (min + max) / 2;
+		input = 0;
+		std::cout << "Is this your number? " << guess << std::endl;
+		std::cout << "1: Higher" << std::endl;
+		std::cout << "2: Lower" << std::endl;
+		std::cout << "3: Yes!" << std::endl;
+		std::cin >> input;
+		if (input == 1)
+		{
+			min = guess + 1;
+		}
+		else if (input == 2)
+		{
+			max = guess - 1;
+		}
+		else if (input == 3)
+		{
+			std::cout << "YAY!! I got it!!" << std::endl;
+
+		}
+	}
+
+	if (input != 3)
+		std::cout << "You dirty liar! I bet I did guess your number!" << std::endl;
 
 	system("pause");
 	return 1;
